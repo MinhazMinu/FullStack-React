91471dc3367a1bc5adf93d91826abbb70c952290
Minhaz <minhaz.taher@gmail.com>
2022-01-04 14:00:49 +0600

    exercises 2.1 to 2.5

---
+++ part 2/test/src/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+function App() {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <h2>App</h2>
+    </div>
+  );
+}
+
+export default App;
